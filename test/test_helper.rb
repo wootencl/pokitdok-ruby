3fa75fa1181505f1f660be0a5749a0642aebf444
John Riney <john.riney@pokitdok.com>
2014-04-04 13:59:16 -0400

    initial commit of jeweler files

---
+++ test/test_helper.rb
@@ -0,0 +1,22 @@
+require 'simplecov'
+
+module SimpleCov::Configuration
+  def clean_filters
+    @filters = []
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_adapter 'test_frameworks'
+end
+
+ENV["COVERAGE"] && SimpleCov.start do
+  add_filter "/.rvm/"
+end
+require 'rubygems'
+require 'test/spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'pokitdok-ruby'
